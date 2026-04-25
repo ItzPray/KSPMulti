1c1b0e4de468e6f98d4fb4a25395a9895ef092e3
ItzPray <theapexskull99@gmail.com>
2026-04-25 03:13:43 -0700

    Clickthrough Popup Fix

--- LmpClient/Base/HarmonyPatcher.cs
+++ LmpClient/Base/HarmonyPatcher.cs
@@ -17,6 +17,7 @@ namespace LmpClient.Base
             }
 
             KerbalKonstructsLaunchPadHarmony.TryRegister(HarmonyInstance);
+            ClickThroughBlockerOneTimePopupHarmony.TryRegister(HarmonyInstance);
         }
     }
 }
